065be2f11553ee86d15bd3fee5b083e12b30a813
cloud <cloudcry@gmail.com>
2012-01-21 00:13:40 +0800

    add renren app key

--- launch.py
+++ launch.py
@@ -76,6 +76,8 @@ class Application(tornado.web.Application):
                 douban_consumer_secret="7524926f6171b225",
                 weibo_app_key="563114544",
                 weibo_app_secret="ac88e78e4c5037839cbbb9c92369bdef",
+                renren_app_key="8f9607b8f2d4446fbc798597dc1dcdd4",
+                renren_app_secret="c8bfb41852ae40589f268007205fce13",
                 debug=True,
                 )
         super(Application, self).__init__(handlers, **settings)
