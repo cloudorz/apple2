a53d68d35686ab1d4a7292e7a066140a14a98cb5
cloud <cloudcry@gmail.com>
2012-02-04 18:07:31 +0800

    pyscript

---
+++ scripts/syncsns.py
@@ -0,0 +1,19 @@
+ # coding:utf-8
+ 
+ import pycurl, json
+ 
+def on_receive(stream):
+    
+    for b in stream.splitlines():
+        try:
+            res = json.loads(b)
+            data = json.loads(res['value'])
+        except:
+            pass
+        else:
+            print data
+ 
+ conn = pycurl.Curl()
+ conn.setopt(pycurl.URL, "http://127.0.0.1:8888/c/snspost")
+ conn.setopt(pycurl.WRITEFUNCTION, on_receive)
+ conn.perform()
