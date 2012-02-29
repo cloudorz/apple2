5a2c113cd5c87538a6536c3d5b807657ac88a319
cloud <cloudcry@gmail.com>
2012-02-29 15:24:31 +0800

    add avvatr list get

--- apps/admin.py
+++ apps/admin.py
@@ -6,6 +6,7 @@ from tornado.web import HTTPError
 from apps import BaseRequestHandler
 from apps.models import User
 from utils.decorator import authenticated, validclient, admin
+from utils.tools import QDict
 
 
 class AdminAvatarHandler(BaseRequestHandler):
@@ -13,12 +14,50 @@ class AdminAvatarHandler(BaseRequestHandler):
     @authenticated
     @admin
     def get(self, uk):
-        user = User.query.get_by_userkey(uk)
-        if user and user.id > 0:
-            info = user.user2dict4right()
-            self.render_json(info)
+        if uk:
+            user = User.query.get_by_userkey(uk)
+            if user and user.id > 0:
+                info = user.user2dict4right()
+                self.render_json(info)
+            else:
+                raise HTTPError(404)
         else:
-            raise HTTPError(404)
+            q = QDict(
+                    q=self.get_argument('q', ""),
+                    sort=self.get_argument('qs'),
+                    start=int(self.get_argument('st')),
+                    num=int(self.get_argument('qn')),
+                    )
+
+            query_users = User.query
+
+            if q.q:
+                query_users = query_users.filter(User.name.like('%'+q.q+'%'))
+
+            # composite the results collection
+            total = query_users.count()
+            query_dict = {
+                    'q': q.q,
+                    'qs': q.sort,
+                    'st': q.start,
+                    'qn': q.num,
+                    }
+
+            user_collection = {
+                    'users': [e.user2dict4right() for e in query_users.order_by(q.sort).limit(q.num).offset(q.start)],
+                    'total': total,
+                    'link': self.full_uri(query_dict),
+                    }
+
+            if q.start + q.num < total:
+                query_dict['st'] = q.start + q.num
+                user_collection['next'] = self.full_uri(query_dict)
+
+            if q.start > 0:
+                query_dict['st'] = max(q.start - q.num, 0)
+                user_collection['prev'] = self.full_uri(query_dict)
+
+            self.render_json(user_collection)
 
     @authenticated
     @admin
